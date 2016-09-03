57f8bf273919d3ba4bcfeae4ceaadb56816b52a7
Majeed Alhassan <aalhassan@madisoncollege.edu>
2016-09-03 11:06:32 -0500

    Initial commit

---
+++ src/main/java/com/paulawaite/controller/Welcome.java
@@ -0,0 +1,32 @@
+package com.paulawaite.controller;
+
+import com.paulawaite.entity.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+/**
+ * A simple servlet to welcome the user.
+ * @author pwaite
+ */
+
+@WebServlet(
+        urlPatterns = {"/welcome"}
+)
+
+public class Welcome extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User("Java", "Superstar", "jr001");
+        req.setAttribute("user", user);
+        System.out.println("Hello World!");
+        System.out.println("it's me, Wombat!");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/welcome.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@include file="head.jsp"%>
+<html>
+<body>
+<h2>Simple Maven Web App Demo</h2>
+<a href = "welcome">Go to the Welcome Servlet</a>
+</body>
+</html>
